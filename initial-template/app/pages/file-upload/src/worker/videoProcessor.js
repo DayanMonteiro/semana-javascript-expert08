5e4118d2294b2715f6b887517f73c7e4faf7afcd
DayanMonteiro <dmsmamb@gmail.com>
2023-10-27 22:00:33 -0300

    render Decoded Frames And Get Encoded Chunks

--- initial-template/app/pages/file-upload/src/worker/videoProcessor.js
+++ initial-template/app/pages/file-upload/src/worker/videoProcessor.js
@@ -9,7 +9,7 @@ export default class VideoProcessor {
     this.#mp4Demuxer = mp4Demuxer;
   }
   /** @returns {ReadableStream} */
-  mp4Decoder(encoderConfig, stream) {
+  mp4Decoder(stream) {
     return new ReadableStream({
       start: async (controller) => {
         const decoder = new VideoDecoder({
@@ -25,7 +25,18 @@ export default class VideoProcessor {
 
         return this.#mp4Demuxer
           .run(stream, {
-            onConfig(config) {
+            async onConfig(config) {
+              const { supported } = await VideoDecoder.isConfigSupported(
+                config
+              );
+              if (!supported) {
+                console.error(
+                  "mp4Muxer VideoDecoder config not supported!",
+                  config
+                );
+                controller.close();
+                return;
+              }
               decoder.configure(config);
             },
             /** @param {EncodedVideoChunk} chunk */
@@ -41,15 +52,104 @@ export default class VideoProcessor {
       },
     });
   }
+
+  enconde144p(encoderConfig) {
+    let _encode;
+    const readable = new ReadableStream({
+      start: async (controller) => {
+        const { supported } = await VideoEncoder.isConfigSupported(
+          encoderConfig
+        );
+        if (!supported) {
+          const message = "encode144p VideoEncoder config not supported!";
+          console.error(message, encoderConfig);
+          controller.error(message);
+          return;
+        }
+        _encode = new VideoEncoder({
+          /**
+           *
+           * @param {EncodedVideoChunk} frame
+           * @param {EncodedVideoChunkMetadata} config
+           */
+          output: (frame, config) => {
+            if (config.decoderConfig) {
+              const decoderConfig = {
+                type: "config",
+                config: config.decoderConfig,
+              };
+              controller.enqueue(decoderConfig);
+            }
+            controller.enqueue(frame);
+          },
+          error: (err) => {
+            console.error("VideoEncoder 144p", err);
+            controller.error(err);
+          },
+        });
+
+        await _encode.configure(encoderConfig);
+      },
+    });
+
+    const writable = new WritableStream({
+      async write(frame) {
+        _encode.encode(frame);
+        frame.close();
+      },
+    });
+
+    return {
+      readable,
+      writable,
+    };
+  }
+
+  renderDecodedFramesAndGetEncodedChunks(renderFrame) {
+    let _decoder;
+    return new TransformStream({
+      start: (controller) => {
+        _decoder = new VideoDecoder({
+          output(frame) {
+            renderFrame(frame);
+          },
+          error(e) {
+            console.error("error at renderFrames", e);
+            controller.error(e);
+          },
+        });
+      },
+      /**
+       *
+       * @param {EncodedVideoChunk} encodedChunk
+       * @param {TransformStreamDefaultController} controller
+       */
+      async transform(encodedChunk, controller) {
+        if (encodedChunk.type === "config") {
+          await _decoder.configure(encodedChunk.config);
+          return;
+        }
+        _decoder.decode(encodedChunk);
+
+        // need the encoded version to use webM
+        controller.enqueue(encodedChunk);
+      },
+    });
+  }
+
   async start({ file, encoderConfig, renderFrame }) {
     const stream = file.stream();
     const fileName = file.name.split("/").pop().replace(".mp4", "");
-    await this.mp4Decoder(encoderConfig, stream).pipeTo(
-      new WritableStream({
-        write(frame) {
-          renderFrame(frame);
-        },
-      })
-    );
+    await this.mp4Decoder(stream)
+      .pipeThrough(this.enconde144p(encoderConfig))
+      .pipeThrough(this.renderDecodedFramesAndGetEncodedChunks(renderFrame))
+      .pipeTo(
+        new WritableStream({
+          write(frame) {
+            // debugger;
+            //  renderFrame(frame);
+          },
+        })
+      );
   }
 }
